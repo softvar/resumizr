b75bebd3655fc9471c9b325e27c6dcffc5384908
softvar <varun2902@gmail.com>
2014-03-01 19:37:21 +0530

    views setup for rendering first template

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+import datetime
+
+def login(request):
+    now = datetime.datetime.now()
+    t = get_template('index.html')
+    html = t.render(Context({'user': 'varun','current_date': now}))
+    return HttpResponse(html)
