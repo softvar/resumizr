dacbdd0dbc1931be9e4e1fe5e2a57b7e174e115e
psych0der <mayankbhola@gmail.com>
2014-03-31 17:25:17 +0530

    added account association after login

--- api/views.py
+++ api/views.py
@@ -8,16 +8,10 @@ from django.contrib.auth import logout as auth_logout
 import datetime
 
 
-# dictionary of social auth mapping
-backends = {
+# list of social auth mapping
+backends = ['twitter','github','facebook','google-oauth2','linkedin-oauth2']
     
-    'twitter' : "{% url 'social:begin' 'twitter' %}",
-    'github' : "{% url 'social:begin' 'github' %}",
-    'facebook' : "{% url 'social:begin' 'facebook' %}",
-    'google-oauth2' : "{% url 'social:begin' 'google-oauth2' %}",
-    'linkedin-oauth2' : "{% url 'social:begin' 'linkedin-oauth2' %}"
-}
-
+    
 
 def home(request):
     '''Home view, displays login mechanism'''
@@ -28,7 +22,7 @@ def home(request):
 
 def login(request):
 	''' login mechanism '''
-	return render(request,'social_auth.html',{'backends' : backends})
+	return render(request,'social_auth.html',{'oauth_providers' : backends})
 
 def logout(request):
     """Logs out user"""
@@ -38,6 +32,14 @@ def logout(request):
 @login_required
 def app(request):
     """Login complete view, displays user data"""
-    #print dir(request.user.social_auth)
 
-    return render(request,'app.html',{'user': request.user , 'backends':backends})
+    # list of associated user auth
+    associated_providers = [oauth.provider for oauth in request.user.social_auth.all()]
+   
+    oauth_providers = []
+
+    for provider in backends :
+        if provider not in associated_providers:
+            oauth_providers.append(provider)
+
+    return render(request,'app.html',{'user': request.user, 'oauth_providers' : oauth_providers})
