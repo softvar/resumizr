5b27f079013691f48f971e1c4bfb50b4cd84aa6a
psych0der <mayankbhola@gmail.com>
2014-04-01 12:57:19 +0530

    made parent directory a python path; added initial registeration form

--- api/views.py
+++ api/views.py
@@ -1,29 +1,54 @@
+import sys
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
+from django.conf import settings
+
+sys.path.append('../..')
+
+from lib.forms import RegisterationForm
 
 import datetime
 
 
 # list of social auth mapping
-backends = ['twitter','github','facebook','google-oauth2','linkedin-oauth2']
-    
-    
+backends = ['twitter', 'github', 'facebook',
+            'google-oauth2', 'linkedin-oauth2']
+
 
 def home(request):
     '''Home view, displays login mechanism'''
     if request.user.is_authenticated():
         return redirect('app')
-    else :
-    	return redirect('login')
+    else:
+        return redirect('login')
+
+
+def register(request):
+    ''' registeration form '''
+
+    if request.method == 'POST':
+        form = RegisterationForm(request.POST)
+        if form.is_valid():
+            return redirect('/thanks/')  # Redirect after POST
+    else:
+        form = RegisterationForm()  # An unbound form
+
+    return render(request, 'register.html', {
+        'form': form,
+    })
+
 
 def login(request):
-	''' login mechanism '''
-	return render(request,'social_auth.html',{'oauth_providers' : backends})
-
+    ''' login mechanism '''
+    if request.user.is_authenticated():
+        return redirect('app')
+    else :
+        return render(request,'social_auth.html',{'oauth_providers' : backends})
+        
 def logout(request):
     """Logs out user"""
     auth_logout(request)
