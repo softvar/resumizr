533d27ca7de598811ef4eaf6a1252752bcd14021
softvar <varun2902@gmail.com>
2014-05-11 16:21:42 +0530

    pre-fetching skills, multiple skill sets typeahead issue fixed

--- static/resumizr/js/cv-generation/generate.js
+++ static/resumizr/js/cv-generation/generate.js
@@ -50,42 +50,9 @@ $(function () {
       this.tagsinput('input').typeahead('setQuery', '');
     }, $('#tagInputs')));*/
 
-    
-    var substringMatcher = function(strs) {
-      return function findMatches(q, cb) {
-        var matches, substringRegex;
-     
-        // an array that will be populated with substring matches
-        matches = [];
-     
-        // regex used to determine if a string contains the substring `q`
-        substrRegex = new RegExp(q, 'i');
-     
-        // iterate through the pool of strings and for any string that
-        // contains the substring `q`, add it to the `matches` array
-        $.each(strs, function(i, str) {
-          if (substrRegex.test(str)) {
-            // the typeahead jQuery plugin expects suggestions to a
-            // JavaScript object, refer to typeahead docs for more info
-            matches.push({ value: str });
-          }
-        });
-     
-        cb(matches);
-      };
-    };
+
+    skillsName = defaultSkills;
  
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
-     
     $('.tagInputs').tagsinput('input').typeahead({
       hint: true,
       highlight: true,
@@ -95,7 +62,7 @@ $(function () {
     {
       name: 'states',
       displayKey: 'value',
-      source: substringMatcher(states)
+      source: substringMatcher(skillsName)
     }).bind('typeahead:selected', $.proxy(function (obj, datum) {  
       this.tagsinput('add', datum.value);
       this.tagsinput('input').typeahead('');
@@ -172,7 +139,7 @@ $(function () {
               name: 'states',
               displayKey: 'value',
               tagClass: 'label label-warning',
-              source: substringMatcher(states)
+              source: substringMatcher(skillsName)
             }).bind('typeahead:selected', $.proxy(function (obj, datum) {  
               this.tagsinput('add', datum.value);
               this.tagsinput('input').typeahead('');
