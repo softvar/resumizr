bb176a4b6159795143d7d9759484ce22d73a837b
softvar <varun2902@gmail.com>
2014-05-31 06:25:46 +0530

    showing different resumes along with last updated dateime

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,10 +8,20 @@ from django.shortcuts import render, redirect
 
 import json
 
+import time
 
 @login_required
-def show_dashboard(request):
+def show_dashboard(request):    
     return render(request, 'dashboard/index.html', {})
+
+@login_required
+def create_new_resume(request):
+    numOfCv = len(request.user.resumizr_data.resume_data['resume']) + 1;
+    now = time.strftime('%x %X')
+    #request.user.resumizr_data.resume_data['resume'] = {}
+    request.user.resumizr_data.resume_data['resume'][str(numOfCv)]= {'UpdatedOn': now}
+    request.user.resumizr_data.save()
+    return HttpResponse(json.dumps({'no':numOfCv}),mimetype='application/json')
 
 @login_required
 def get_all_resumes(request):
@@ -20,8 +30,10 @@ def get_all_resumes(request):
     _json = {}
     for x in xrange(1,numOfCv + 1):
         x = str(x)
-        _json[x] = request.user.resumizr_data.resume_data['resume'][x]['UpdatedOn']
-
+        try:
+            _json[x] = request.user.resumizr_data.resume_data['resume'][x]['UpdatedOn']
+        except:
+            _json[x] = ''
     return HttpResponse(json.dumps(_json),mimetype='application/json')
         #return HttpResponse(json.dumps(request.user.resumizr_data.resume_data['resume']),mimetype='application/json')
     #except :
