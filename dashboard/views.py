675e09973a42f006704428ab7d04d35daa5ade6b
softvar <varun2902@gmail.com>
2014-05-31 20:35:54 +0530

    real-time resume listing

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,17 +9,41 @@ from django.shortcuts import render, redirect
 import json
 
 import time
-
+# list of social auth mapping
+backends = ['twitter', 'github', 'facebook',
+            'google-oauth2', 'linkedin-oauth2']
+            
 @login_required
-def show_dashboard(request):    
-    return render(request, 'dashboard/index.html', {})
+def show_dashboard(request):
+    try:
+        request.user.resumizr_data
+    except ObjectDoesNotExist:
+        request.user.resumizr_data = ResumizrUserData(user = request.user)
+        request.user.resumizr_data.save()
+    
+
+
+    # list of associated user auth
+    associated_providers = [
+        oauth.provider for oauth in request.user.social_auth.all()]
+
+    oauth_providers = []
+
+    for provider in backends:
+        if provider not in associated_providers:
+            oauth_providers.append(provider)
+
+    context = {'user': request.user, 'oauth_providers': oauth_providers}
+    return render(request, 'dashboard/index.html', context)
 
 @login_required
 def create_new_resume(request):
     numOfCv = len(request.user.resumizr_data.resume_data['resume']) + 1;
     now = time.strftime('%x %X')
+    data = []
+    data.append(now)
     #request.user.resumizr_data.resume_data['resume'] = {}
-    request.user.resumizr_data.resume_data['resume'][str(numOfCv)]= {'UpdatedOn': now}
+    request.user.resumizr_data.resume_data['resume'][str(numOfCv)]= {'UpdatedOn': data}
     request.user.resumizr_data.save()
     return HttpResponse(json.dumps({'no':numOfCv}),mimetype='application/json')
 
